d66f53c0a93fcc41ec0af9bbf0e8d50e477903d9
Frederick Lebel <frederick.lebel@gmail.com>
2014-10-13 23:54:08 -0400

    - Added auth function (partial).

--- src/Alveo/SDK.php
+++ src/Alveo/SDK.php
@@ -11,4 +11,15 @@ class SDK
     {
         $this->config = new Configuration($params);
     }
+
+    public function auth($params)
+    {
+        $headers = array();
+        $req = Requests::get($this->getEndpoint(), $headers, $params);
+    }
+
+    private function getEndpoint()
+    {
+        return $this->config['endpoint'];
+    }
 } 
